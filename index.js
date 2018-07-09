b9923e37781d11beda9640e4017a70e6f995c8d2
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-09 16:08:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,35 +3,52 @@ var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
 
+var sql = require('mssql')
+var sqlInstance = require("mssql")
+var nodemailer = require('nodemailer')
+
 app.use(bodyParser.json())
 
-app.set('port', (process.env.PORT || 4000))
+app.set('port', (process.env.PORT || 7777))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+var dbConfig = {
+    user: 'sa',
+    password: 'p@ssw0rd1234',
+    server: 'demomagic2.southeastasia.cloudapp.azure.com', 
+    database: 'LinebotDB',
+    port:1433,
+    options: {
+        encrypt: false // Use this if you're on Windows Azure
+    }                      
+};
+
 app.post('/webhook', (req, res) => {
-  var text = req.body.events[0].message.text
+  var msg = req.body.events[0].message.text
   var sender = req.body.events[0].source.userId
   var replyToken = req.body.events[0].replyToken
   console.log(text, sender, replyToken)
   console.log(typeof sender, typeof text)
   // console.log(req.body.events[0])
-  if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
-    sendText(sender, text)
+  if (msg === 'สวัสดี' || msg === 'Hello' || msg === 'hello') {
+    sendText(sender, msg)
   }
   res.sendStatus(200)
-})
+}    )
 
-function sendText (sender, text) {
+function sendText (sender, msg) {
   let data = {
     to: sender,
     messages: [
       {
         type: 'text',
-        text: 'สวัสดี'
+        text: 'สวัสดีค่ะ'
       }
     ]
   }
+
+  
   request({
     headers: {
       'Content-Type': 'application/json',

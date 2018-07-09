342a457425dd44f37d6db8acdafa7dd664882a83
TechnocomTnc <predapon.t@gmail.com>
2018-07-09 10:34:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
 
+app.use(bodyParser.json())
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.get('/', function(request, response) {
   response.send('Hello Worldaaaaaa!')

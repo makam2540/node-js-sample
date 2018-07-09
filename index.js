0186833b781b8b102e3fcde63daa1fe58ca36599
TechnocomTnc <predapon.t@gmail.com>
2018-07-09 10:12:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
+app.post('/webhook', (req, res) => {
+    res.sendStatus(200)
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })

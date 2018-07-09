3d6e5c1f88d1d3610dba9ed2239b8e6e2ada29b1
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-09 14:52:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
+    var sender = req.body.events[0].source.userId
+
     reply(reply_token, msg)
+    console.log(typeof sender, typeof text)
+
     res.sendStatus(200)
 })
+
 app.listen(port)
 function reply(reply_token, msg) {
     let headers = {
@@ -18,20 +23,23 @@ function reply(reply_token, msg) {
         'Authorization': 'Bearer {Rz8z1ee8jjPGKgYsiVruxdBDpWA4ryYEh5QKu7KLtb4o1HN3h38LHyWUEoWYOGVolNmGP1fFw7UbxocelHU/0Y/j+b2/jch/cpqEW6dhyi8smlFI+vsQVttuzLtCZPHm5K7MNg39sFK7Z8jWxhv7ngdB04t89/1O/w1cDnyilFU=}'
     }
     
-    if(msg === 'สวัสดี'){   
-    let body = JSON.stringify({
-        replyToken: reply_token,
-        messages: [{
-            type: 'text',
-            text: 'How are you?'
-        }]
-    })
-    }else{
-         messages: [{
-            type: 'text',
-            text: '.....'
-        }]
+    if (msg === 'สวัสดี') {
+      sendText(sender, text)
     }
+    res.sendStatus(200)
+  }
+        
+  function sendText (sender, text) {
+    let data = {
+      to: sender,
+      messages: [
+        {
+          type: 'text',
+          text: 'สวัสดีค่ะ'
+        }
+      ]
+    }
+
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
         headers: headers,

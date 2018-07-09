6508c8ed68b11d15e5f47e50af8ff6dda2ffd72c
TechnocomTnc <predapon.t@gmail.com>
2018-07-09 10:53:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,31 +27,31 @@ app.post('/webhook', (req, res) => {
   res.sendStatus(200)
 })
 
-function sendText (sender, text) {
-  let data = {
-    to: sender,
-    messages: [
-      {
-        type: 'text',
-        text: 'สวัสดีค่ะ เราเป็นผู้ช่วยปรึกษาด้านความรัก'
-      }
-    ]
-  }
-  request({
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer 7YR60AJ855Zu1Etxsc7aCdFqhip1o8yAKj7PzLe90ClE9Po0fz5o81BeghtpCki4+zFZ7FrYjjbrFvQw84+Axi+P1zWPnxSCTl/lF5gVTDaDqdC5IHk30qnjo7GQ1hHKizexgGNpBPn/Fwz3slJqkQdB04t89/1O/w1cDnyilFU='
-    },
-    url: 'https://api.line.me/v2/bot/message/reply',
-    method: 'POST',
-    body: data,
-    json: true
-  }, function (err, res, body) {
-    if (err) console.log('error')
-    if (res) console.log('success')
-    if (body) console.log(body)
-  })
-}
+// function sendText (sender, text) {
+//   let data = {
+//     to: sender,
+//     messages: [
+//       {
+//         type: 'text',
+//         text: 'สวัสดีค่ะ เราเป็นผู้ช่วยปรึกษาด้านความรัก'
+//       }
+//     ]
+//   }
+//   request({
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Authorization': 'Bearer 7YR60AJ855Zu1Etxsc7aCdFqhip1o8yAKj7PzLe90ClE9Po0fz5o81BeghtpCki4+zFZ7FrYjjbrFvQw84+Axi+P1zWPnxSCTl/lF5gVTDaDqdC5IHk30qnjo7GQ1hHKizexgGNpBPn/Fwz3slJqkQdB04t89/1O/w1cDnyilFU='
+//     },
+//     url: 'https://api.line.me/v2/bot/message/reply',
+//     method: 'POST',
+//     body: data,
+//     json: true
+//   }, function (err, res, body) {
+//     if (err) console.log('error')
+//     if (res) console.log('success')
+//     if (body) console.log(body)
+//   })
+// }
 
 
 app.listen(app.get('port'), function() {

e1d68205e90ff2964c30d48329dfc94dcf5c0276
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-10 14:24:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -55,13 +55,16 @@ function sendText (sender, msg) {
                 var req = new sql.Request(conn);
                 req.query('SELECT * FROM Question',function (err, result, fields) {
                  
+                  if(text == result.recordset[0].q_topic){
+                        var id = result.recordset[0].q_Id;
+                  }
                      // recordset.recordset[0].q_Id;
                   let data = {
                     to: sender,
                     messages: [
                       {
                         type: 'text',
-                        text: result.recordset[0].q_Id
+                        text: id
                       }
                     ]
                   }

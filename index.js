8616a25b7595ac83411fe6354fa11cd000086121
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-10 09:42:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,99 +1,34 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
-var app = express()
-
-var sql = require('mssql');
-var sqlInstance = require("mssql");
-
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+const port = process.env.PORT || 4000
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
-app.set('port', (process.env.PORT || 4000))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-
-
-var port = process.env.PORT || 7777;
-
-// parse application/json
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
-
-// connect to your database
-
- var dbConfig = {
-                      user: 'sa',
-                      password: 'P@ssw0rd1234',
-                      server: 'demomagic2.southeastasia.cloudapp.azure.com', 
-                      database: 'LinebotDB',
-                      port:1433,
-                      options: {
-                          encrypt: false // Use this if you're on Windows Azure
-                      }                      
-    };
-
 app.post('/webhook', (req, res) => {
-  var text = req.body.events[0].message.text
-  var sender = req.body.events[0].source.userId
-  var replyToken = req.body.events[0].replyToken
-  console.log(text, sender, replyToken)
-  console.log(typeof sender, typeof text)
-  // console.log(req.body.events[0])
-
-
-  var conn = new sql.ConnectionPool(dbConfig);
-    conn.connect().then(function () {
-                  var req = new sql.Request(conn);
-                  req.query('SELECT * FROM Question').then(function (recordset) {
-                    res.send(recordset);
-                    sendText(sender, text)                       
-                  })
-                  .catch(function (err) {
-                      conn.close();
-                      res.send(err);
-                  });        
+    let reply_token = req.body.events[0].replyToken
+    let msg = req.body.events[0].message.text
+    reply(reply_token, msg)
+    res.sendStatus(200)
+})
+app.listen(port)
+function reply(reply_token, msg) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {xxxxxxx}'
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: msg
+        }]
     })
-    .catch(function (err) {
-        res.send(err);
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
     });
-
-
-  // if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
-  //   sendText(sender, text)
-  // }
-  res.sendStatus(200)
-
-
-})
-
-function sendText (sender, text) {
-  let data = {
-    to: sender,
-    messages: [
-      {
-        type: 'text',
-        text: 'สวัสดีค่ะ'
-      }
-    ]
-  }
-  request({
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer Rz8z1ee8jjPGKgYsiVruxdBDpWA4ryYEh5QKu7KLtb4o1HN3h38LHyWUEoWYOGVolNmGP1fFw7UbxocelHU/0Y/j+b2/jch/cpqEW6dhyi8smlFI+vsQVttuzLtCZPHm5K7MNg39sFK7Z8jWxhv7ngdB04t89/1O/w1cDnyilFU='
-    },
-    url: 'https://api.line.me/v2/bot/message/push',
-    method: 'POST',
-    body: data,
-    json: true
-  }, function (err, res, body) {
-    if (err) console.log('error')
-    if (res) console.log('success')
-    if (body) console.log(body)
-  })
 }
-
-app.listen(app.get('port'), function () {
-  console.log('run at port', app.get('port'))
-})

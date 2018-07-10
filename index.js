6e6f753bf1001937dd1fe560fa5d58a1feca1a1b
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-10 09:57:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,19 +34,19 @@ app.use(bodyParser.urlencoded({
     };
 
 app.post('/webhook', (req, res) => {
-  var text = req.body.events[0].message.text
+  var msg = req.body.events[0].message.text
   var sender = req.body.events[0].source.userId
   var replyToken = req.body.events[0].replyToken
   console.log(text, sender, replyToken)
   console.log(typeof sender, typeof text)
   // console.log(req.body.events[0])
-  if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
-    sendText(sender, text)
+  if (msg === 'สวัสดี' || msg === 'Hello' || msg === 'hello') {
+    sendText(sender, msg)
   }
   res.sendStatus(200)
 })
 
-function sendText (sender, text) {
+function sendText (sender, msg) {
   let data = {
     to: sender,
     messages: [

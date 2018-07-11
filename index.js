334851103463df302c91efbae0c1ed836cdf694e
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-11 15:27:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,8 @@ const client = new line.Client({
   channelAccessToken: 'Rz8z1ee8jjPGKgYsiVruxdBDpWA4ryYEh5QKu7KLtb4o1HN3h38LHyWUEoWYOGVolNmGP1fFw7UbxocelHU/0Y/j+b2/jch/cpqEW6dhyi8smlFI+vsQVttuzLtCZPHm5K7MNg39sFK7Z8jWxhv7ngdB04t89/1O/w1cDnyilFU='
 });
 
-        var text1 = getMessageContent(text)
+client.getMessageContent(text)    
+// var text1 = getMessageContent(text)
   .then((stream) => {
     stream.on('data', (chunk) => {
         sendText(sender, text1)

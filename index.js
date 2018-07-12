b459bd949111043915cd7e047eb3c182a8648484
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-12 11:38:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -62,42 +62,41 @@ const client = new line.Client({
 
 function sendText (sender, msg) {
 
-
             client.getMessageContent(msg)
             .then((stream) => {
               stream.on('data', (chunk) => {
       
-                          let data = {
-                            to: sender,
-                            messages: [
-                              {
-                                type: "text",
-                                text : chunk
-                                
-                              }
-                            ]
-                          }
-                  
-                  request({
-                    headers: {
-                      'Content-Type': 'application/json',
-                      'Authorization': 'Bearer Rz8z1ee8jjPGKgYsiVruxdBDpWA4ryYEh5QKu7KLtb4o1HN3h38LHyWUEoWYOGVolNmGP1fFw7UbxocelHU/0Y/j+b2/jch/cpqEW6dhyi8smlFI+vsQVttuzLtCZPHm5K7MNg39sFK7Z8jWxhv7ngdB04t89/1O/w1cDnyilFU='
-                    },
-                    url: 'https://api.line.me/v2/bot/message/push',
-                    method: 'POST',
-                    body: data,
-                    json: true
-                  } , 
-                    function (err, res, body) {
-                    if (err) console.log('error')
-                    if (res) console.log('success')
-                    if (body) console.log(body)
-                  })   
+                    var a = chunk       
 
 
 });
 stream.on('error', (err) => {
-  // error handling
+  let data = {
+    to: sender,
+    messages: [
+      {
+        type: "text",
+        text : chunk
+        
+      }
+    ]
+  }
+
+request({
+headers: {
+'Content-Type': 'application/json',
+'Authorization': 'Bearer Rz8z1ee8jjPGKgYsiVruxdBDpWA4ryYEh5QKu7KLtb4o1HN3h38LHyWUEoWYOGVolNmGP1fFw7UbxocelHU/0Y/j+b2/jch/cpqEW6dhyi8smlFI+vsQVttuzLtCZPHm5K7MNg39sFK7Z8jWxhv7ngdB04t89/1O/w1cDnyilFU='
+},
+url: 'https://api.line.me/v2/bot/message/push',
+method: 'POST',
+body: data,
+json: true
+} , 
+function (err, res, body) {
+if (err) console.log('error')
+if (res) console.log('success')
+if (body) console.log(body)
+})  
 });
 });
 
@@ -106,4 +105,4 @@ stream.on('error', (err) => {
 app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'))
 })
-fhgfgnxgv
+

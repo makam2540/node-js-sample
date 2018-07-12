48685a9602cac4c172bbfd540fe3329ec7c64cd2
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-12 13:37:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,41 +33,6 @@ app.use(bodyParser.urlencoded({
                       server: 'demomagic2.southeastasia.cloudapp.azure.com', 
                       database: 'LinebotDB',
                       port:1433,
-                      options: {var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
-var app = express()
-
-const line = require('@line/bot-sdk');
-
-var Q_id
-var topic
-
-var sql = require('mssql')
-var sqlInstance = require("mssql")
-
-app.use(bodyParser.json())
-
-app.set('port', (process.env.PORT || 4000))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-
-var port = process.env.PORT || 7777;
-
-// parse application/json
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
-
-// connect to your database
-
- var dbConfig = {
-                      user: 'sa',
-                      password: 'P@ssw0rd1234',
-                      server: 'demomagic2.southeastasia.cloudapp.azure.com', 
-                      database: 'LinebotDB',
-                      port:1433,
                       options: {
                           encrypt: true // Use this if you're on Windows Azure
                       }                      
@@ -88,22 +53,22 @@ app.post('/webhook', (req, res) => {
 })
 
 const client = new line.Client({
-  channelAccessToken: 'Rz8z1ee8jjPGKgYsiVruxdBDpWA4ryYEh5QKu7KLtb4o1HN3h38LHyWUEoWYOGVolNmGP1fFw7UbxocelHU/0Y/j+b2/jch/cpqEW6dhyi8smlFI+vsQVttuzLtCZPHm5K7MNg39sFK7Z8jWxhv7ngdB04t89/1O/w1cDnyilFU='
+  channelAccessToken : 'Rz8z1ee8jjPGKgYsiVruxdBDpWA4ryYEh5QKu7KLtb4o1HN3h38LHyWUEoWYOGVolNmGP1fFw7UbxocelHU/0Y/j+b2/jch/cpqEW6dhyi8smlFI+vsQVttuzLtCZPHm5K7MNg39sFK7Z8jWxhv7ngdB04t89/1O/w1cDnyilFU='
 });
 
 function sendText (sender, msg) {
 
-  client.getMessageContent(msg)
-  .then((stream) => {
-    stream.on('data', (chunk) => {
+  // client.getMessageContent(msg)
+  // .then((stream) => {
+  //   stream.on('data', (chunk) => {
+
                           let data = {
                             to: sender,
                             messages: [
                               {
                                 type: "text",
                                 text : msg
-                                // originalContentUrl: "https://ih1.redbubble.net/image.449196043.4904/st%2Csmall%2C215x235-pad%2C210x230%2Cf8f8f8.lite-1u1.jpg" ,
-                                // previewImageUrl: "https://ih1.redbubble.net/image.449196043.4904/st%2Csmall%2C215x235-pad%2C210x230%2Cf8f8f8.lite-1u1.jpg"
+                                
                               }
                             ]
                           }
@@ -112,6 +77,7 @@ function sendText (sender, msg) {
                     headers: {
                       'Content-Type': 'application/json',
                       'Authorization': 'Bearer Rz8z1ee8jjPGKgYsiVruxdBDpWA4ryYEh5QKu7KLtb4o1HN3h38LHyWUEoWYOGVolNmGP1fFw7UbxocelHU/0Y/j+b2/jch/cpqEW6dhyi8smlFI+vsQVttuzLtCZPHm5K7MNg39sFK7Z8jWxhv7ngdB04t89/1O/w1cDnyilFU='
+                      
                     },
                     url: 'https://api.line.me/v2/bot/message/push',
                     method: 'POST',
@@ -123,74 +89,11 @@ function sendText (sender, msg) {
                     if (body) console.log(body)
                   })   
 
-                });
-                stream.on('error', (err) => {
-                  // error handling
-                });
-              });
-
-
-}
-app.listen(app.get('port'), function () {
-  console.log('run at port', app.get('port'))
-})
-
-
-app.post('/webhook', (req, res) => {
-//   var text = req.body.events[0].message.text
-  var text = req.body.events[0].message.id
-  var sender = req.body.events[0].source.userId
-  var replyToken = req.body.events[0].replyToken
-
-  console.log(text, sender, replyToken)
-  console.log(typeof sender, typeof text)
-  
-    sendText(sender, text)
-
-  res.sendStatus(200)
-})
-
-const client = new line.Client({
-  channelAccessToken: 'Rz8z1ee8jjPGKgYsiVruxdBDpWA4ryYEh5QKu7KLtb4o1HN3h38LHyWUEoWYOGVolNmGP1fFw7UbxocelHU/0Y/j+b2/jch/cpqEW6dhyi8smlFI+vsQVttuzLtCZPHm5K7MNg39sFK7Z8jWxhv7ngdB04t89/1O/w1cDnyilFU='
-});
-
-function sendText (sender, msg) {
-
-  client.getMessageContent('<messageId>')
-  .then((stream) => {
-    stream.on('data', (chunk) => {
-                          let data = {
-                            to: sender,
-                            messages: [
-                              {
-                                type: "text",
-                                text : msg
-                                // originalContentUrl: "https://ih1.redbubble.net/image.449196043.4904/st%2Csmall%2C215x235-pad%2C210x230%2Cf8f8f8.lite-1u1.jpg" ,
-                                // previewImageUrl: "https://ih1.redbubble.net/image.449196043.4904/st%2Csmall%2C215x235-pad%2C210x230%2Cf8f8f8.lite-1u1.jpg"
-                              }
-                            ]
-                          }
-                  
-                  request({
-                    headers: {
-                      'Content-Type': 'application/json',
-                      'Authorization': 'Bearer Rz8z1ee8jjPGKgYsiVruxdBDpWA4ryYEh5QKu7KLtb4o1HN3h38LHyWUEoWYOGVolNmGP1fFw7UbxocelHU/0Y/j+b2/jch/cpqEW6dhyi8smlFI+vsQVttuzLtCZPHm5K7MNg39sFK7Z8jWxhv7ngdB04t89/1O/w1cDnyilFU='
-                    },
-                    url: 'https://api.line.me/v2/bot/message/push',
-                    method: 'POST',
-                    body: data,
-                    json: true
-                  }, function (err, res, body) {
-                    if (err) console.log('error')
-                    if (res) console.log('success')
-                    if (body) console.log(body)
-                  })   
-
-                });
-                stream.on('error', (err) => {
-                  // error handling
-                });
-              });
+              //   });
+              //   stream.on('error', (err) => {
+              //     // error handling
+              //   });
+              // });
 
 
 }

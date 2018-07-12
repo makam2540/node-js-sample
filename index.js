e8382d105d830d37f4841054f75cc45ad0ddc9fa
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-12 10:56:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ var app = express()
 
 const line = require('@line/bot-sdk');
 
-const client = new line.Client({
-    channelAccessToken: 'Rz8z1ee8jjPGKgYsiVruxdBDpWA4ryYEh5QKu7KLtb4o1HN3h38LHyWUEoWYOGVolNmGP1fFw7UbxocelHU/0Y/j+b2/jch/cpqEW6dhyi8smlFI+vsQVttuzLtCZPHm5K7MNg39sFK7Z8jWxhv7ngdB04t89/1O/w1cDnyilFU='
-  });
-
 var Q_id
 var topic
 
@@ -21,63 +17,62 @@ app.set('port', (process.env.PORT || 4000))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-var port = process.env.PORT || 7777;
+// var port = process.env.PORT || 7777;
 
-// parse application/json
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
+// // parse application/json
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+// 	extended: true
+// }));
 
-// connect to your database
+// // connect to your database
 
- var dbConfig = {
-                      user: 'sa',
-                      password: 'P@ssw0rd1234',
-                      server: 'demomagic2.southeastasia.cloudapp.azure.com', 
-                      database: 'LinebotDB',
-                      port:1433,
-                      options: {
-                          encrypt: true // Use this if you're on Windows Azure
-                      }                      
-    };
+//  var dbConfig = {
+//                       user: 'sa',
+//                       password: 'P@ssw0rd1234',
+//                       server: 'demomagic2.southeastasia.cloudapp.azure.com', 
+//                       database: 'LinebotDB',
+//                       port:1433,
+//                       options: {
+//                           encrypt: true // Use this if you're on Windows Azure
+//                       }                      
+//     };
+
+
+const client = new line.Client({
+  
+  channelAccessToken: 'Rz8z1ee8jjPGKgYsiVruxdBDpWA4ryYEh5QKu7KLtb4o1HN3h38LHyWUEoWYOGVolNmGP1fFw7UbxocelHU/0Y/j+b2/jch/cpqEW6dhyi8smlFI+vsQVttuzLtCZPHm5K7MNg39sFK7Z8jWxhv7ngdB04t89/1O/w1cDnyilFU='
+});
 
 app.post('/webhook', (req, res) => {
-  var text = req.body.events[0].message.id
-//   var text = req.body.events[0].message.image
+//   var text = req.body.events[0].message.text
+  var text = req.body.events[0].message.type
   var sender = req.body.events[0].source.userId
   var replyToken = req.body.events[0].replyToken
 
   console.log(text, sender, replyToken)
   console.log(typeof sender, typeof text)
-
-  client.getMessageContent(text)
-     .then((stream) => {
-        stream.on('data', (chunk) => {
-            sendText(sender, text)
-    });
-        stream.on('error', (err) => {
-        // error handling
-    });
-    });
+  
+    sendText(sender, text)
 
   res.sendStatus(200)
 })
 
 
-
-
-
-
 function sendText (sender, msg) {
-                    
 
-                            let data = {
+
+  client.getMessageContent(msg)
+  .then((stream) => {
+    stream.on('data', (chunk) => {
+      
+                          let data = {
                             to: sender,
                             messages: [
                               {
-                                type: "image",
-                                text : "ssssss"
+                                type: "text",
+                                text : chunk
+                                
                               }
                             ]
                           }
@@ -95,13 +90,16 @@ function sendText (sender, msg) {
                     if (err) console.log('error')
                     if (res) console.log('success')
                     if (body) console.log(body)
-                  })
+                  })   
 
 
-              
-                   
-                }
-                             
+});
+stream.on('error', (err) => {
+  // error handling
+});
+});
+
+}
 
 app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'))

92d5441bda9c9cf709a092858e0f9ae5e75e8a01
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-13 10:09:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const line = require('@line/bot-sdk');
 
 var Q_id
 var topic
+var chunks = [];
 
 var sql = require('mssql')
 var sqlInstance = require("mssql")
@@ -65,40 +66,43 @@ function sendText (sender, msg) {
   });
 
   client.getMessageContent(msg)
-    //  setEncoding(msg)
+
   .then((stream) => {
     stream.on('data', (chunk) => {
 
-      // data1 += chunk
-                          let data = {
-                            to: sender,
-                            messages: [
-                              {
-                                type: "text",
-                                text :  ''+ chunk
-                                
-                              }
-                            ]
-                          }
-                  
-                  request({
-                    headers: {
-                      'Content-Type': 'application/json',
-                      'Authorization': 'Bearer Rz8z1ee8jjPGKgYsiVruxdBDpWA4ryYEh5QKu7KLtb4o1HN3h38LHyWUEoWYOGVolNmGP1fFw7UbxocelHU/0Y/j+b2/jch/cpqEW6dhyi8smlFI+vsQVttuzLtCZPHm5K7MNg39sFK7Z8jWxhv7ngdB04t89/1O/w1cDnyilFU='
-                    },
-                    url: 'https://api.line.me/v2/bot/message/push',
-                    method: 'POST',
-                    body: data,
-                    json: true
-                  }, function (err, res, body) {
-                    if (err) console.log('error')
-                    if (res) console.log('success')
-                    if (body) console.log(body)
-                  })    // end request
+      chunks.push(chunk)
+                          
 
                 });
-                stream.on('error', (err) => {
-                  // error handling
+                stream.on('end', (err) => {
+
+                  var buffer = Buffer.concat(chunks)
+
+                  let data = {
+                    to: sender,
+                    messages: [
+                      {
+                        type: "text",
+                        text :  ''+ chunks
+                        
+                      }
+                    ]
+                  }
+          
+          request({
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer Rz8z1ee8jjPGKgYsiVruxdBDpWA4ryYEh5QKu7KLtb4o1HN3h38LHyWUEoWYOGVolNmGP1fFw7UbxocelHU/0Y/j+b2/jch/cpqEW6dhyi8smlFI+vsQVttuzLtCZPHm5K7MNg39sFK7Z8jWxhv7ngdB04t89/1O/w1cDnyilFU='
+            },
+            url: 'https://api.line.me/v2/bot/message/push',
+            method: 'POST',
+            body: data,
+            json: true
+          }, function (err, res, body) {
+            if (err) console.log('error')
+            if (res) console.log('success')
+            if (body) console.log(body)
+          })    // end request
                 });
               });
 
